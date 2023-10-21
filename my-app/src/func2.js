ab2d51842a9941bec874675764f26f780703c51c
kelvin <kelvin92831@gmail.com>
2023-10-22 00:13:24 +0800

    satisfy new DS in db

--- my-app/src/func2.js
+++ my-app/src/func2.js
@@ -6,26 +6,29 @@ import {initializeApp} from 'firebase/app';
 // import {signInWithEmailAndPassword, createUsesrWithEmailAndPassword, getAuth, signInWithPopup, GoogleAuthProvider, signOut} from "firebase/auth";
 import { getDatabase, ref, push, child, onValue, val, get, set } from "firebase/database";
 
-const firebaseConfig2 = {
-    apiKey: "AIzaSyDurUQg6c7MENG1Ce7qIl-KNhlg0cSd-KU",
-    authDomain: "nxp-2-889f2.firebaseapp.com",
-    databaseURL: "https://nxp-2-889f2-default-rtdb.firebaseio.com",
-    projectId: "nxp-2-889f2",
-    storageBucket: "nxp-2-889f2.appspot.com",
-    messagingSenderId: "972237774704",
-    appId: "1:972237774704:web:f6619eb33767dfead9b9b5"
+const firebaseConfig = {
+  apiKey: "AIzaSyBb1s3xaRqk1JUGq88g5H9-oINz23Ca-uA",
+  authDomain: "lab12-34dd3.firebaseapp.com",
+  databaseURL: "https://lab12-34dd3-default-rtdb.firebaseio.com",
+  projectId: "lab12-34dd3",
+  storageBucket: "lab12-34dd3.appspot.com",
+  messagingSenderId: "185579286263",
+  appId: "1:185579286263:web:7b49551753c8bbc8806df2"
 };
-var app2 = initializeApp(firebaseConfig2, "other");
+var app = initializeApp(firebaseConfig);
 // const auth = getAuth();
-let database2 = getDatabase(app2);
+let database = getDatabase(app);
+
+const Ref = ref(database, 'light');
+
 
 function Func2page(){
 
     const [TestSet] = useState([
-        {id: 1, straightSignal: "red", lateralSignal: "green", time:"2023-09-22-10:22:30"},
-        {id: 2, straightSignal: "red", lateralSignal: "green", time:"2023-09-22-10:22:40"},
-        {id: 3, straightSignal: "red", lateralSignal: "green", time:"2023-09-22-10:22:40"},
-        {id: 4, straightSignal: "green", lateralSignal: "red", time:"2023-09-22-10:22:40"},
+        {id: 1, situation: "NS_Straight_Green", time:"2023-09-22-10:22:30"},
+        {id: 2, situation: "NS_Straight_Green", time:"2023-09-22-10:22:40"},
+        {id: 3, situation: "NS_Straight_Green", time:"2023-09-22-10:22:40"},
+        {id: 4, situation: "NS_Straight_Green", time:"2023-09-22-10:22:40"},
 
 
     ])
@@ -51,7 +54,7 @@ function Func2page(){
     const fetchData = async() => {
         let newSatusSet=[];
 
-        onValue(ref(database2), (snapshot) => {
+        onValue(Ref, (snapshot) => {
             newSatusSet.length = 0; 
             snapshot.forEach((item) => {
               newSatusSet.push(item.val()); 
@@ -62,35 +65,32 @@ function Func2page(){
 
 
     useEffect(() => {
-        //console.log(StatusSet);
+        console.log(StatusSet);
     }, [StatusSet])
 
 
-    let len = StatusSet.length;
-    console.log(len);
 
-    let index = len;
-    const foundData = StatusSet.find(item=>item.id==index)
+
+
     
-    if(foundData) {
-        console.log(foundData);
-        return(
-            <div className='func2Page'>
-              <div className='title'><h1>紅燈秒數調整</h1></div>
-              <div><img></img></div>
-              <div className='situation'>{foundData.status}</div>
-              <div class='d-flex justify-content-center align-items-center'>
-                <div className='content'>直向燈號: {foundData.straightSignal}</div>
-                <div style={{ width: '150px' }}></div>
-                <div className='content'>橫向燈號: {foundData.lateralSignal}</div>
-              </div> 
+    
+        
+    return(
+        <div className='func2Page'>
+          <div className='title'><h1>紅燈秒數調整</h1></div>
+          <ul>
+            {StatusSet.map(item => (
+                <li key={item.id}>
+                <div>ID: {item.id}</div>
+                <div>Time: {item.time}</div>
+                <div>Situation: {item.situation}</div>
+                </li>
+            ))}
+          </ul>
+        </div>
+    );
 
-            </div>
-        );
-
-    }else{
-        console.log('未找到數據')
-    }
+  
 
 
 }
